8c6a99f3e5eb75bfa926cc5efca4648708da4c9c
Mauricio Assis <mauricioassisrt@gmail.com>
2026-02-26 21:53:36 -0300

    Aula 20 concluida: persistencia e CRUD de usuarios

---
+++ src/main/java/br/com/projeto/domain/model/UserEntity.java
@@ -0,0 +1,41 @@
+package br.com.projeto.domain.model;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="users")
+public class UserEntity extends PanacheEntityBase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name="name")
+    private String name;
+    @Column(name="age")
+    private Integer age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
--- src/main/java/br/com/projeto/rest/UserResource.java
+++ src/main/java/br/com/projeto/rest/UserResource.java
@@ -1,10 +1,15 @@
 package br.com.projeto.rest;
 
+import br.com.projeto.domain.model.UserEntity;
 import br.com.projeto.rest.dto.CreateUserRequest;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
+import jakarta.transaction.Transactional;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.Application;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import org.h2.engine.User;
 
 @Path("/users")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -12,12 +17,44 @@ import jakarta.ws.rs.core.Response;
 public class UserResource {
 
     @POST
-    public Response createUser(CreateUserRequest userRequest){
-        return Response.ok(userRequest).build();
+    @Transactional
+    public Response createUser(CreateUserRequest userRequest) {
+        UserEntity user = new UserEntity();
+        user.setName(userRequest.getName());
+        user.setAge(userRequest.getAge());
+        user.persist();
+        return Response.ok(user).build();
     }
 
     @GET
-    public Response listAllUsers(){
-        return Response.ok().build();
+    public Response listAllUsers() {
+        PanacheQuery<UserEntity> query = UserEntity.findAll();
+        return Response.ok(query.list()).build();
+    }
+
+    @DELETE
+    @Path("{id}")
+    @Transactional
+    public Response delete(@PathParam("id") Long id) {
+        UserEntity user = UserEntity.findById(id);
+        if (user != null) {
+            user.delete();
+            return Response.ok().build();
+        }
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
+
+    @PUT
+    @Path("{id}")
+    @Transactional
+    public Response atualizar(@PathParam("id") Long id, CreateUserRequest userRequest) {
+        UserEntity user = UserEntity.findById(id);
+        if (user != null) {
+            user.setAge(userRequest.getAge());
+            user.setName(userRequest.getName());
+
+            return Response.ok().build();
+        }
+        return Response.status(Response.Status.NOT_FOUND).build();
     }
 }
